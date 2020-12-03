1fb6188cb65762e45e5f954b18f50cae2a4ab758
Rene Wang <rene.wang1@hotmail.com>
2020-12-03 04:16:23 +0000

    cleaned up fetcher file

--- fetcher.js
+++ fetcher.js
@@ -5,10 +5,7 @@ const site = process.argv.slice(2)[0];
 const path = process.argv.slice(2)[1];
 
 
-request(site, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
+request(site, (body) => {
   fs.appendFile(path, body, (err) => {
     if (err) throw err;
     fs.stat(path, (err, stats) => {
